5f5678f483b9c1a9f494604241f9369ec3b1d7ae
Aman Prajapati <167050491+amanprajapati333@users.noreply.github.com>
2026-06-12 22:59:20 +0530

    Add files via upload

---
+++ Summer_Assignmengt_2501920109003/Week_2/Day_5/Task1.java
@@ -0,0 +1,51 @@
+
+
+public class Task1 {
+
+   
+        public String longestPalindrome(String s) {
+
+            if (s == null || s.length() < 1) {
+                return "";
+            }
+
+            int start = 0, end = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+
+                int len1 = expandFromCenter(s, i, i);       // Odd length
+                int len2 = expandFromCenter(s, i, i + 1);   // Even length
+
+                int len = Math.max(len1, len2);
+
+                if (len > end - start) {
+                    start = i - (len - 1) / 2;
+                    end = i + len / 2;
+                }
+            }
+
+            return s.substring(start, end + 1);
+        }
+
+        private int expandFromCenter(String s, int left, int right) {
+
+            while (left >= 0 && right < s.length()
+                    && s.charAt(left) == s.charAt(right)) {
+
+                left--;
+                right++;
+            }
+
+            return right - left - 1;
+        }
+    
+
+    public static void main(String[] args) {
+
+        Task1 obj=new Task1();
+
+        String s = "babad";
+
+        System.out.println("Longest Palindrome: " + obj.longestPalindrome(s));
+    }
+}
